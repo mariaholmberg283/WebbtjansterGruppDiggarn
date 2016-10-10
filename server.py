b854c5d3674dfde1b7d7c1593d72b2a69980492a
Niclas661 <niclas.sv@gmail.com>
2016-10-10 11:51:14 +0200

    skapat server
    kan göra requests till sr:s api, visa upp basic info inom /test

---
+++ server.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from flask import Flask, render_template
+import requests, xmltodict
+from xml.etree import ElementTree
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/test')
+def testAPI():
+    r = requests.get("http://api.sr.se/api/v2/playlists/rightnow?channelid=2576")
+    newDict = xmltodict.parse(r.content)
+    return newDict['sr']['playlist']['song']['description']
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
+
+testAPI()
