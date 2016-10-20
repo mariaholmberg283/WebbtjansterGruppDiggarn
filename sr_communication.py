afbcbdda7ce62bde80ecef313470294af2865091
theresenn <therese.nielsen0@gmail.com>
2016-10-20 16:16:18 +0200

    Comments added

--- sr_communication.py
+++ sr_communication.py
@@ -1,12 +1,16 @@
 import requests, xmltodict
 
 def getChannels():
-    r = requests.get("http://api.sr.se/api/v2/channels/")
+    ''' This function gets all the channels and saves them in a dictionairy and returns the channels '''
+
+    r = requests.get("http://api.sr.se/api/v2/channels/") 
     channelsDict = xmltodict.parse(r.content)
-    formattedDict = channelsDict['sr']['channels']
+    formattedDict = channelsDict['sr']['channels'] 
     return formattedDict
 
 def getChannel(channelID):
+    ''' This function get the channels' id so that we can choose channel and get current info '''
+
     urlLink = "http://api.sr.se/api/v2/channels/" + str(channelID)
     r = requests.get(urlLink)
     channelDict = xmltodict.parse(r.content)
@@ -14,6 +18,9 @@ def getChannel(channelID):
     return formattedDict
 
 def getPlaying(channelID):
+    ''' This function gets the channels' playlist which includes information about the current song and artist. 
+    If there is no information available an error-message will appear '''
+
     urlLink = "http://api.sr.se/api/v2/playlists/rightnow?channelid=" + str(channelID)
     r = requests.get(urlLink)
     playlistDict = xmltodict.parse(r.content)
@@ -23,13 +30,11 @@ def getPlaying(channelID):
     else:
         return {'error': 'No information available'}
 
-try:
-    print getPlaying(223)['description']
-except:
-    print "N/A"
-
     
 def nextPlaying(channelID):
+     ''' This function gets the channels' playlist which includes information about the upcoming song and artist.
+     If there is no information available an error-message will appear '''
+
     urlLink = "http://api.sr.se/api/v2/playlists/rightnow?channelid=" + str(channelID)
     r = requests.get(urlLink)
     playlistDict = xmltodict.parse(r.content)
