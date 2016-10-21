6796fbfe56e0ea25740f4f617e79191333df8815
theresenn <therese.nielsen0@gmail.com>
2016-10-21 12:52:52 +0200

    Comments

--- spotify_communication.py
+++ spotify_communication.py
@@ -1,6 +1,8 @@
 import requests, json
 
 def getSong(artist, title):
+    ''' This function creates a search of the artist and title. By using Spotify's API we can search through Spotify's library and return the result. If there is no match en error-message will appear. '''
+
     searchQuery = artist + "%20" + title
     searchQuery = searchQuery.replace(" ", "%20")
     print searchQuery
@@ -14,6 +16,8 @@ def getSong(artist, title):
 
 
 def authorize():
+    ''' This function '''
+
     r = requests.get("https://accounts.spotify.com/authorize?client_id=69e933979e0946a8acb19513ae0bc9c1&response_type=code&redirect_uri=localhost:5000/spotifyResponse&scope=user-read-private")
     print r.content
 
