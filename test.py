2dda0d4ab44aabcd1c6242d57445045a78b9c8a6
theresenn <therese.nielsen0@gmail.com>
2016-10-27 16:44:05 +0200

    rensning

--- test.py
+++
@@ -1,7 +0,0 @@
-import requests
-import xmltodict, json
-
-r = requests.get("http://api.sr.se/api/v2/playlists/rightnow?channelid=2576")
-x = r.content
-o = xmltodict.parse(x)
-print o['sr']['playlist']['song']['description']
